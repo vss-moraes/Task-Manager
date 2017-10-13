88b5ddb351456158d91c0e672d96faec12012da2
Vinicius Moraes <vsantana0212@gmail.com>
2017-10-13 17:08:40 -0400

    Criar controller e rotas para terefas

---
+++ app/controllers/tarefas_controller.rb
@@ -0,0 +1,51 @@
+class TarefasController < ApplicationController
+  before_action :set_tarefa, only: [:show, :update, :destroy]
+
+  # GET /tarefas
+  def index
+    @tarefas = Tarefa.all
+
+    render json: @tarefas
+  end
+
+  # GET /tarefas/1
+  def show
+    render json: @tarefa
+  end
+
+  # POST /tarefas
+  def create
+    @tarefa = Tarefa.new(tarefa_params)
+
+    if @tarefa.save
+      render json: @tarefa, status: :created, location: @tarefa
+    else
+      render json: @tarefa.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /tarefas/1
+  def update
+    if @tarefa.update(tarefa_params)
+      render json: @tarefa
+    else
+      render json: @tarefa.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /tarefas/1
+  def destroy
+    @tarefa.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_tarefa
+      @tarefa = Tarefa.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def tarefa_params
+      params.fetch(:tarefa, {})
+    end
+end
